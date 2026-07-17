5841bd24ad1673dd9c06701b9c4bc6af80bf6192
Cian O <46230102+wiggleforlife@users.noreply.github.com>
2026-07-16 21:30:05 -0700

    semicolon

--- modules/default.nix
+++ modules/default.nix
@@ -35,7 +35,7 @@ in
               description = "Minimum duration of the PlymouthVista animation in seconds.";
             };
           };
-          description = "plymouth settings"
+          description = "plymouth settings";
         };
         sddm.enable = lib.mkEnableOption "the SDDM theme";
       };
